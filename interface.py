f0bcfe3a189adcfa2a9cc765fcc79085bdb73fc8
Maxime <maximemasson65@gmail.com>
2024-11-22 17:21:43 +0100

    feat(interface): spécifier l'url

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@ message = st.text_area("Entrez votre message ici")
 
 # Bouton d'analyse.
 def analyser_sentiment(message):
-    url = 'votre_url_api'
+    url = 'https://badbuzzwebapp-f3brayd9dmgfcsb8.canadacentral-01.azurewebsites.net/'
     data = {'message': message}
     response = requests.post(url, json=data)
     prediction = response.json()['prediction']
