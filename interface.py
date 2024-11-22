c4deb05fb0c5ebe6bef0299ee690c5bf773ce2b6
Maxime <maximemasson65@gmail.com>
2024-11-22 17:10:15 +0100

    Premier commit

---
+++ interface.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+# Titre de l'app.
+st.title("Analyseur de Sentiment")
+
+# Zone de text.
+message = st.text_area("Entrez votre message ici")
+
+# Bouton d'analyse.
+def analyser_sentiment(message):
+    url = 'votre_url_api'
+    data = {'message': message}
+    response = requests.post(url, json=data)
+    prediction = response.json()['prediction']
+    return prediction
+
+
+if st.button("Analyser le sentiment"):
+    prediction = analyser_sentiment(message)
+    st.write("Le sentiment est : ", prediction)
