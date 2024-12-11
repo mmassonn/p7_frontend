b355c6ae486d4000f1a02f63c2c038b26b0e99d0
Maxime <maximemasson65@gmail.com>
2024-12-11 14:52:18 +0100

    changer terme message par text

--- interface.py
+++ interface.py
@@ -10,7 +10,7 @@ message = st.text_area("Entrez votre message ici")
 # Bouton d'analyse.
 def analyser_sentiment(message):
     url = 'badbuzzwebapp-f3brayd9dmgfcsb8.canadacentral-01.azurewebsites.net'
-    data = {'message': message}
+    data = {'text': message}
     response = requests.post(url, json=data)
     prediction = response.json()['prediction']
     return prediction
