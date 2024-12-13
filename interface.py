1cf24b99d856a9d6fb7abdbdaa63ff9581e271f0
mmassonn <54768326+mmassonn@users.noreply.github.com>
2024-12-13 15:43:08 +0100

    print(response)

--- interface.py
+++ interface.py
@@ -12,6 +12,7 @@ def analyser_sentiment(message):
     url = 'https://badbuzzwebapp-f3brayd9dmgfcsb8.canadacentral-01.azurewebsites.net/'
     data = {'text': message}
     response = requests.post(url, json=data)
+    print(response)
     prediction = response.json()['prediction']
     return prediction
 
